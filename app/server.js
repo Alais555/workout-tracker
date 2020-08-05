7fdeab28c64b35aa816e9411a8327e13d8a91d69
Michael Alais <michaelgct555@gmail.com>
2020-08-05 14:08:25 -0700

    server

--- app/server.js
+++ app/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const logger = require("morgan");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
+const routes = require("./routes/api-routes");
+const routes = require("./routes/html-routes");
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT | 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
